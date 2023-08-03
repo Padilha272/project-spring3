f72a96c68a049b8f2b7ce1ddf3cd2bef50f18692
Matheus Padilha <matheuspadilha48@hotmail.com>
2023-08-03 14:55:48 -0300

    User Insert

--- src/main/java/com/matheuspadilha/project/resources/UserResource.java
+++ src/main/java/com/matheuspadilha/project/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.matheuspadilha.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheuspadilha.project.entities.User;
 import com.matheuspadilha.project.services.UserService;
@@ -31,6 +35,18 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	/*@RequestBody is used to indicate to the spring boot that the parameter "obj"
+	 * is an object that will be insert in the body of the HTTp request and will
+	 * be converted in a User object
+	 * 
+	 * 
+	 */
 	
 	/*
 	@GetMapping(value = "/nome")
