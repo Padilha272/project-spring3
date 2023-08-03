b805afc1db10b36604aa70e7e39c7d750ca1a1c9
Matheus Padilha <matheuspadilha48@hotmail.com>
2023-08-03 15:06:29 -0300

    User delete

--- src/main/java/com/matheuspadilha/project/resources/UserResource.java
+++ src/main/java/com/matheuspadilha/project/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,14 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	/*@RequestBody is used to indicate to the spring boot that the parameter "obj"
 	 * is an object that will be insert in the body of the HTTp request and will
 	 * be converted in a User object
--- src/main/java/com/matheuspadilha/project/services/UserService.java
+++ src/main/java/com/matheuspadilha/project/services/UserService.java
@@ -28,6 +28,8 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
-	
+	public void delete(Long id) {		
+		repository.deleteById(id);
+	}
 	
 }
