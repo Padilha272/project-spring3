5947b9aedd287bbdbe55904b39c294bb22b6a541
Matheus Padilha <matheuspadilha48@hotmail.com>
2023-08-07 10:35:41 -0300

    Exception - findById

--- src/main/java/com/matheuspadilha/project/services/UserService.java
+++ src/main/java/com/matheuspadilha/project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.matheuspadilha.project.entities.User;
 import com.matheuspadilha.project.repositories.UserRepository;
+import com.matheuspadilha.project.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()->new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
